b45fac850ff217d662034678f004a0073b02687d
Valery <valery.kutsak@outlook.com>
2020-08-01 00:19:35 +0300

    Add project files.

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    class Gender
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Create new gender.
+        /// </summary>
+        /// <param name="Name">Name.</param>
+        public Gender(string Name)
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new ArgumentException("The gender can't be Null or WhiteSpace", nameof(Name));
+            }
+            this.Name = Name;
+        }
+        /// <summary>
+        /// Get Name.
+        /// </summary>
+        /// <returns>Name.</returns>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Fitness.BL/Model/User.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    class User
+    {
+        #region properties
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Gender.
+        /// </summary>
+        public Gender Gender { get; }
+        /// <summary>
+        /// BirthDate.
+        /// </summary>
+        public DateTime Birth { get; }
+        /// <summary>
+        /// Weight.
+        /// </summary>
+        public double Weight { get; }
+        /// <summary>
+        /// Height.
+        /// </summary>
+        public double Height { get; }
+        #endregion
+        /// <summary>
+        /// Create new user.
+        /// </summary>
+        /// <param name="name">Name.</param>
+        /// <param name="gender">Gender.</param>
+        /// <param name="birth">BirthDate.</param>
+        /// <param name="weight">Weight.</param>
+        /// <param name="height">Height.</param>
+        public User(string name,
+                    Gender gender,
+                    DateTime birth,
+                    double weight,
+                    double height)
+        {
+            #region check_input
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("The name can't be Null or WhiteSpace.", nameof(name));
+            }
+            if (gender == null)
+            {
+                throw new ArgumentException("Gender can't be null.", nameof(gender));
+            }
+
+            if (birth < DateTime.Parse("01.01.1938") || birth > DateTime.Now)
+            {
+                throw new ArgumentException("Unbelieveble date of birth.", nameof(birth));
+            }
+            if (height <= 0)
+            {
+                throw new ArgumentException("Height can't be <=0.", nameof(height));
+            }
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Weight can't be <=0.", nameof(weight));
+            }
+            #endregion
+            Name = name;
+            Gender = gender;
+            Birth = birth;
+            Weight = weight;
+            Height = height;
+        }
+        /// <summary>
+        /// Get user info.
+        /// </summary>
+        /// <returns>Name, gender, birthdate, weight, height.</returns>
+        public override string ToString()
+        {
+            return $"Name:{Name}, Gender:{Gender}, BirthDate:{Birth}, Weight: {Weight}, Height:{Height}.";
+        }
+    }
+}
