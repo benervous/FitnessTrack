1b830cc8bb375a22d5542fc025dc190d176639d9
Valery <valery.kutsak@outlook.com>
2020-08-04 02:29:56 +0300

    added eating controller

--- Fitness.BL/Controller/EatingController.cs
+++ Fitness.BL/Controller/EatingController.cs
@@ -8,39 +8,99 @@ namespace Fitness.BL.Controller
 {
     class EatingController:ControllerBase
     {
+        #region properties.
+        /// <summary>
+        /// Food file name.
+        /// </summary>
         private const string FILE_NAME_FOOD = "food.dat";
+        /// <summary>
+        /// Eating file name.
+        /// </summary>
         private const string FILE_NAME_EATING = "eating.dat";
+        /// <summary>
+        /// User.
+        /// </summary>
         public readonly User User;
+        /// <summary>
+        /// Food collection.
+        /// </summary>
         public List<Food> Food { get; }
-        public List<Eating> Eating { get; }
+        /// <summary>
+        /// Eating.
+        /// </summary>
+        public Eating Eating { get; }
+        #endregion
+        /// <summary>
+        /// Eating controller.
+        /// </summary>
+        /// <param name="User"></param>
         public EatingController(User User)
         {
             this.User = User ?? throw new ArgumentNullException(("User can't be null"), nameof(User));
             Food = GetAllFood();
-            Eating = GetAllEating();
+            Eating = GetEating();
         }
-        private List<Eating> GetAllEating()
+        /// <summary>
+        /// Get Eating.
+        /// </summary>
+        /// <returns>Eating dictionary.</returns>
+        private Eating GetEating()
         {
-            return Load<List<Eating>>(FILE_NAME_EATING) ?? new List<Eating>();
+            return Load<Eating>(FILE_NAME_EATING) ?? new Eating(User);
         }
+        /// <summary>
+        /// Get Food Collection.
+        /// </summary>
+        /// <returns>Food list.</returns>
         private List<Food> GetAllFood()
         {
             return Load<List<Food>>(FILE_NAME_FOOD) ?? new List<Food>();
         }
+        /// <summary>
+        /// Save Data.
+        /// </summary>
         private void Save()
         {
             Save(FILE_NAME_FOOD, Food);
             Save(FILE_NAME_EATING, Eating);
         }
-        //public bool Add(string Name, double weight)
-        //{
-        //    var food = Food.SingleOrDefault(f => f.Name == Name);
-        //    var eating = new Eating(User);
-        //    if(food != null)
-        //    {
-        //        Eating.Add();
-        //    }
+        /// <summary>
+        /// Add food by it's name.
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="weight"></param>
+        /// <returns>True.</returns>
+        public bool Add(string Name, double weight)
+        {
+            var food = Food.SingleOrDefault(f => f.Name == Name);
+            if (food != null)
+            {
+                Eating.Add(food, weight);
+                Save();
+                return true;
+            }
+            return false;
 
-        //}
+        }
+        /// <summary>
+        /// Add food by its type.
+        /// </summary>
+        /// <param name="Food"></param>
+        /// <param name="weight"></param>
+        public void Add(Food Food, double weight)
+        {
+            var product = this.Food.SingleOrDefault(f=>f.Name == Food.Name);
+            if(product == null)
+            {
+                this.Food.Add(Food);
+                Eating.Add(Food, weight);
+                Save();
+            }
+            else
+            {
+                Eating.Add(product, weight);
+                Save();
+            }
+        }
     }
 }
